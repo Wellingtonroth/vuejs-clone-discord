5f15ade8258d285f320cca63e05649b1fbb968c8
Wellington Roth <wellington_roth@hotmail.com>
2020-11-08 20:32:06 -0300

    initiated

---
+++ src/Components/ServerList/ServerButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="serverbutton" :class="{'serverbutton-hasNotification' : hasNotification}">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selected: Boolean,
+    isHome: Boolean,
+    hasNotification: Boolean,
+    mentions: Number
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.serverbutton {
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  margin-bottom: 8px;
+  background-color: var(--primary);
+  cursor: pointer;
+  position: relative;
+  border-radius: 50%;
+  transition: .2s;
+
+  &.active,
+  &:hover {
+    border-radius: 16px;
+    background-color: var(--discord);
+  }
+}
+
+.serverbutton-hasNotification {
+  &::before {
+    content: "";
+    width: 9px;
+    height: 9px;
+    background-color: var(--white);
+    position: absolute;
+    left: -17px;
+    top: calc(50% - 4.5px);
+    border-radius: 50%;
+    display: block;
+  }
+}
+</style>
---
+++ src/Components/ServerList/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <ServerButton isHome/>
+
+    <div class="separator">
+      <ServerButton :mentions="3" />
+      <ServerButton hasNotification />
+      <ServerButton :mentions="32" hasNotification />
+      <ServerButton :mentions="4"/>
+      <ServerButton hasNotification/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ServerButton from "./ServerButton";
+
+export default {
+  components: {
+    ServerButton
+  }
+};
+
+</script>
+
+<style scoped lang="scss">
+
+.container {
+  grid-area: SL;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--tertiary);
+  padding: 11px 0;
+  max-height: 100vh;
+}
+
+.separator {
+  width: 48px;
+  border-bottom: solid 2px var(--quaternary);
+  margin-bottom: 8px;
+}
+
+</style>
